58fa7c7ebdd636e47765c2a82df3e9f2917ab91a
d4rkpn5rk1bk <d4rkpn5rk1bk@gmail.com>
2022-03-18 15:53:10 +0500

    Command Options

---
+++ CommandLine/ScanPageData.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebParser.CommandLine
+{
+    [Verb("scan-data", HelpText = "Display pages list")]
+    internal class ScanPageData
+    {
+        [Option('a', "all", HelpText = "Scans all pages content")]
+        public bool AllPages { get; set; }
+
+        [Option('r', "raw", HelpText = "Scans page content as raw html")]
+        public bool Raw { get; set; }
+
+        public static int Run(ScanPageData options) 
+        {
+            return 0;
+        }
+    }
+}
